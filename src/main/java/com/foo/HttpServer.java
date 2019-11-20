8f5b7a0351dddf729c679f192d26230db7ae47ff
liyan <rockleeprc@outlook.com>
2019-11-20 23:13:13 +0800

    feat:SimpleChannelInboundHandler内方法调用过程

--- src/main/java/com/foo/HttpServer.java
+++ src/main/java/com/foo/HttpServer.java
@@ -1,6 +1,7 @@
 package com.foo;
 
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
@@ -15,24 +16,31 @@ import java.net.InetSocketAddress;
 
 public class HttpServer {
     public static void main(String[] args) {
-        ServerBootstrap bootstrap = new ServerBootstrap();
-        EventLoopGroup boss = new NioEventLoopGroup();
-        EventLoopGroup work = new NioEventLoopGroup();
-        int prot = 8899;
+        ServerBootstrap bootstrap = null;
+        EventLoopGroup boss=null;
+        EventLoopGroup work = null;
+        int port = 8899;
 
         try {
+            bootstrap = new ServerBootstrap();
+            boss = new NioEventLoopGroup();
+            work = new NioEventLoopGroup();
+
             bootstrap.group(boss, work)
                     .handler(new LoggingHandler(LogLevel.DEBUG)) // 配置handler日志级别
                     .channel(NioServerSocketChannel.class)// 配置使用的channel
                     .childHandler(new HttpServerInitializer()); // 配置初始化器
-            ChannelFuture f = bootstrap.bind(new InetSocketAddress(prot)).sync();
-            System.out.println(" server start up on port : " + prot);
-            f.channel().closeFuture().sync();
+            ChannelFuture channelFuture = bootstrap.bind(new InetSocketAddress(port)).sync();
+            channelFuture.channel().closeFuture().sync();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
-            boss.shutdownGracefully();
-            work.shutdownGracefully();
+            if (boss != null) {
+                boss.shutdownGracefully();
+            }
+            if (work != null) {
+                work.shutdownGracefully();
+            }
         }
     }
 
@@ -45,32 +53,64 @@ public class HttpServer {
         protected void initChannel(SocketChannel ch) throws Exception {
             ChannelPipeline pipeline = ch.pipeline();
             // 将组建添加到ChannelPipeline的最后
-            pipeline.addLast(new HttpServerCodec());// http 编解码
+            pipeline.addLast("httpServerCodec",new HttpServerCodec());// http 编解码
             pipeline.addLast("httpAggregator", new HttpObjectAggregator(512 * 1024)); // http 消息聚合器                                                                     512*1024为接收的最大contentlength
-            pipeline.addLast("httpRequestHandle", new HttpRequestHandler());// 自定义请求处理器
+            pipeline.addLast("httpServerHandle", new HttpServerHandler());// 自定义请求处理器
         }
     }
 
     /**
      * 请求处理器，初始化器所需要的组件
      */
-    private static class HttpRequestHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
+    private static class HttpServerHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
 
         @Override
         protected void channelRead0(ChannelHandlerContext ctx, FullHttpRequest msg) throws Exception {
+            System.out.println(msg.getClass());
+
             if (msg instanceof HttpRequest) {
+
+                ByteBuf content = Unpooled.copiedBuffer("hello netty", CharsetUtil.UTF_8);
                 FullHttpResponse response = new DefaultFullHttpResponse(
                         HttpVersion.HTTP_1_1,
                         HttpResponseStatus.OK,
-                        Unpooled.copiedBuffer("hello netty", CharsetUtil.UTF_8));
+                        content);
                 response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/html; charset=UTF-8");
+                response.headers().set(HttpHeaderNames.CONTENT_LENGTH,content.readableBytes());
+
                 ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
+                ctx.close();
             }
         }
 
         @Override
         public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
             ctx.flush();
+        }
+
+        @Override
+        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+            super.channelActive(ctx);
+        }
+
+        @Override
+        public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+            super.channelRegistered(ctx);
+        }
+
+        @Override
+        public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+            super.handlerAdded(ctx);
+        }
+
+        @Override
+        public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+            super.channelInactive(ctx);
+        }
+
+        @Override
+        public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+            super.channelUnregistered(ctx);
         }
     }
 }
