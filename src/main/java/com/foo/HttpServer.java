34bbe7579a78477486668462a0790b0aa3adbc1b
rockleeprc <rockleeprc@outlook.com>
2019-11-25 09:46:01 +0800

    edit

--- src/main/java/com/foo/HttpServer.java
+++ src/main/java/com/foo/HttpServer.java
@@ -13,11 +13,12 @@ import io.netty.handler.logging.LoggingHandler;
 import io.netty.util.CharsetUtil;
 
 import java.net.InetSocketAddress;
+import java.net.URI;
 
 public class HttpServer {
     public static void main(String[] args) {
         ServerBootstrap bootstrap = null;
-        EventLoopGroup boss=null;
+        EventLoopGroup boss = null;
         EventLoopGroup work = null;
         int port = 8899;
 
@@ -53,7 +54,7 @@ public class HttpServer {
         protected void initChannel(SocketChannel ch) throws Exception {
             ChannelPipeline pipeline = ch.pipeline();
             // 将组建添加到ChannelPipeline的最后
-            pipeline.addLast("httpServerCodec",new HttpServerCodec());// http 编解码
+            pipeline.addLast("httpServerCodec", new HttpServerCodec());// http 编解码
             pipeline.addLast("httpAggregator", new HttpObjectAggregator(512 * 1024)); // http 消息聚合器                                                                     512*1024为接收的最大contentlength
             pipeline.addLast("httpServerHandle", new HttpServerHandler());// 自定义请求处理器
         }
@@ -71,13 +72,18 @@ public class HttpServer {
 
             if (msg instanceof HttpRequest) {
 
+                HttpRequest req = (HttpRequest) msg; //将对象强制类型转换为HttpRequest的对象
+                URI uri = new URI(req.uri()); //java.net包下的
+                if ("/favicon.ico".equals(uri.getPath())) {
+                    System.out.println("get favicon.icon");
+                    return;
+                }
+
                 ByteBuf content = Unpooled.copiedBuffer("hello netty", CharsetUtil.UTF_8);
                 FullHttpResponse response = new DefaultFullHttpResponse(
-                        HttpVersion.HTTP_1_1,
-                        HttpResponseStatus.OK,
-                        content);
+                        HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
                 response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/html; charset=UTF-8");
-                response.headers().set(HttpHeaderNames.CONTENT_LENGTH,content.readableBytes());
+                response.headers().set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
 
                 ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
                 ctx.close();
