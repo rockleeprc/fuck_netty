f59c8101f584ac7a43121df3b548f8d9e167876d
rockleeprc <rockleeprc@outlook.com>
2019-11-29 16:48:47 +0800

    edit

--- src/main/java/com/foo/io/bio/BioServerThreadPool.java
+++ src/main/java/com/foo/io/bio/BioServerThreadPool.java
@@ -38,6 +38,8 @@ public class BioServerThreadPool {
             }
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            threadpool.shutdown();
         }
     }
 }
